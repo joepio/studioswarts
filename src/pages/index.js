fe70194965d8b37a31224198ee36c5b537d9595e
Joep Meindertsma <joepmeindertsma@gmail.com>
2019-09-15 13:10:36 +0200

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import pic01 from '../assets/images/pic01.jpg'
+import pic01 from '../assets/images/jupiler1.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Studio Swarts - spatial design"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
@@ -31,45 +31,45 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>Aliquam</h3>
+                                <h3>Festivals</h3>
                                 <p>Ipsum dolor sit amet</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/festivals" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
-                                <h3>Tempus</h3>
+                                <h3>Interior</h3>
                                 <p>feugiat amet tempus</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/festivals" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
-                                <h3>Magna</h3>
+                                <h3>Fashion</h3>
                                 <p>Lorem etiam nullam</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/festivals" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
                                 <h3>Ipsum</h3>
                                 <p>Nisl sed aliquam</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/festivals" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic05})`}}>
                             <header className="major">
                                 <h3>Consequat</h3>
                                 <p>Ipsum dolor sit amet</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/festivals" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic06})`}}>
                             <header className="major">
                                 <h3>Etiam</h3>
                                 <p>Feugiat amet tempus</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/festivals" className="link primary"></Link>
                         </article>
                     </section>
                     <section id="two">
