e7fba1728fbc50e9d53584bec741143e29d5ef71
Joep Meindertsma <joep@argu.co>
2019-12-29 13:07:55 +0100

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,42 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import logo from '../assets/logo.svg'
+import styled from 'styled-components'
+
+const LogoLink = styled(Link)``
+
+const LogoImg = styled.img`
+  height: 100%;
+`
+
+const NavBar = styled.header`
+  position: absolute;
+  height: 2rem;
+  z-index: 10;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+`
+
+const NavLinks = styled.div`
+  margin-left: auto;
+`
+
+const LinkItem = styled(Link)`
+  color: blue;
+`
 
 const Header = props => (
-  <header
-    id="header"
-    className="alt"
-    style={{
-      position: 'absolute',
-      height: '2rem',
-      padding: '1rem',
-      zIndex: 10,
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        color: 'white',
-      }}
-    >
-      <strong>Studio Swarts</strong>
-    </Link>
-  </header>
+  <NavBar>
+    <LogoLink to="/">
+      <LogoImg src={logo} alt="Studio Swarts logo" />
+    </LogoLink>
+    <NavLinks>
+      <LinkItem>over</LinkItem>
+      <LinkItem>hallo</LinkItem>
+    </NavLinks>
+  </NavBar>
 )
 
 export default Header
