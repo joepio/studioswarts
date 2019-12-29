3206582eb2cdee36e3e3a6a5337872c8fb6d1ed1
Joep Meindertsma <joep@argu.co>
2019-12-29 16:59:00 +0100

    Column for index, hover effects

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-background-image'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 const types = {
   title: PropTypes.string,
@@ -9,34 +10,48 @@ const types = {
   src: PropTypes.string,
 }
 
-const imgStyle = {
-  width: '100%',
-}
+const Header = styled.h2`
+  color: white;
+  position: absolute;
+  font-size: 2rem;
+  bottom: 1rem;
+  left: 1rem;
+  z-index: 1;
+`
 
-const headerStyle = {
-  color: 'white',
-  position: 'absolute',
-  fontSize: '2rem',
-  bottom: '1rem',
-  left: '1rem',
-}
+const Wrapper = styled(Link)`
+  width: calc(33.333% - 1rem);
+  position: relative;
+  margin-bottom: 2rem;
+  overflow: hidden;
 
-const previewStyle = {
-  maxWidth: '22rem',
-  position: 'relative',
-  margin: '1rem',
-  flexBasis: '19rem',
-}
+  &:nth-child(3n+1) { order: 1; }
+  &:nth-child(3n+2) { order: 2; }
+  &:nth-child(3n)   { order: 3; }
+`
+
+const BackImg = styled.img`
+  width: 100%;
+  transition: transform .2s;
+
+  &:hover {
+    transform: scale(1.1)
+  }
+`
+
+const BackImgFluid = styled(Img)`
+  width: 100%;
+`
 
 /** Fixed width image with header text links to Project page */
 const ProjectPreview = props => (
-  <Link to={props.url} style={previewStyle}>
-    <h2 style={headerStyle}>{props.title}</h2>
-    {props.src && <img alt={props.title} style={imgStyle} src={props.src} />}
+  <Wrapper to={props.url}>
+    <Header>{props.title}</Header>
+    {props.src && <BackImg alt={props.title} src={props.src} />}
     {props.fluid && (
-      <Img alt={props.title} style={imgStyle} fluid={props.fluid} />
+      <BackImgFluid alt={props.title} fluid={props.fluid} />
     )}
-  </Link>
+  </Wrapper>
 )
 
 ProjectPreview.propTypes = types
