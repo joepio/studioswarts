ab20556581768836bc39c262ebe2dda9448f1a99
Joep Meindertsma <joep@argu.co>
2019-12-30 18:01:36 +0100

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,15 +23,7 @@ import zalando from '../assets/images/projects/zalando/01.jpg'
 import ProjectPreview from '../components/ProjectPreview'
 import Columns from '../components/Columns'
 
-// const filename = (fullPath) => {
-//   const withExtension = fullPath.replace(/^.*[\\\/]/, '')
-//   return withExtension.substr(0, withExtension.lastIndexOf('.'))
-// }
-
 const HomeIndex = ({ data }) => {
-  // console.log('data', data)
-  // const allProjects = data.allMarkdownRemark.edges
-  // const allProjects = data
   return (
     <Layout>
       <Helmet
@@ -43,16 +35,6 @@ const HomeIndex = ({ data }) => {
       ></Helmet>
       <Carousel />
       <Columns>
-        {/* {allProjects.map((project) => {
-          console.log('project', project)
-          return (
-            <ProjectPreview
-              title={project.node.frontmatter.title}
-              src={budCover}
-              url={`projects/${filename(project.node.fileAbsolutePath)}`}
-            />
-          )
-        })} */}
         <ProjectPreview title="Budweiser PR" src={budPR} url="/projects/budPR" />
         <ProjectPreview title="Casper KaDeWe" src={casperBerlin} url="/projects/casperBerlin" />
         <ProjectPreview title="Casper SleepTour" src={casperSleep} url="/projects/casperSleep" />
@@ -73,51 +55,5 @@ const HomeIndex = ({ data }) => {
     </Layout>
   )
 }
-
-// export const squareImage = graphql`
-//   fragment squareImage on File {
-//     childImageSharp {
-//       fluid(maxWidth: 200, maxHeight: 200) {
-//         ...GatsbyImageSharpFluid
-//       }
-//     }
-//   }
-// `
-
-//  \/assets\/images\/(\S+)\/1\.jpg
-// export const pageQuery = graphql`
-//   query {
-//     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___title] }) {
-//       edges {
-//         node {
-//           id
-//           rawMarkdownBody
-//           fileAbsolutePath
-//           id
-//           excerpt(pruneLength: 250)
-//           frontmatter {
-//             title
-//           }
-//         }
-//       }
-//     }
-//     # allImageSharp(filter: { id: { regex: "/assets/images/budPR/1\\.jpg" } }) {
-//     #   edges {
-//     #     node {
-//     #       id
-//     #     }
-//     #   }
-//     # }
-//     budPR: file(relativePath: { eq: "/assets/images/projects/budPR/1.jpg" }) {
-//       ...squareImage
-//     }
-//     image2: file(relativePath: { eq: "images/image2.jpg" }) {
-//       ...squareImage
-//     }
-//     image3: file(relativePath: { eq: "images/image3.jpg" }) {
-//       ...squareImage
-//     }
-//   }
-// `
 
 export default HomeIndex
